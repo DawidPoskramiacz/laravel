8d1544777865f683d61a29a92b3994953e286e3a
Dawid Czarodziej <7057675u@zstzbaszynek.pl>
2023-10-13 15:53:07 +0200

    wip

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TravelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,10 +11,13 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| be assigned to the "api" middleware group. Make some thing great!
 |
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('travels', [TravelController::class, 'index']);
+Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
