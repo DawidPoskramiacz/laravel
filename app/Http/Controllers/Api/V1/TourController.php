dfc87043fa7b6bc6789c776d9fdbdfcf79fd913d
Dawid Czarodziej <7057675u@zstzbaszynek.pl>
2023-10-13 16:03:04 +0200

    wip

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -6,14 +6,16 @@ use App\Models\Travel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TourResource;
+use App\Http\Requests\ToursListRequest;
 
 class TourController extends Controller
 {
-    public function index(Travel $travel)
+    public function index(Travel $travel, ToursListRequest $request)
     {
         $tours = $travel->tours()
-            ->orderBy('starting_date')
-            ->paginate();
+        ->when($request->priceFrom, function ($query) use ($request){
+            $query->where('price', '>=', $request->priceFrom*100);
+        });
 
         return TourResource::collection($tours);
     }
